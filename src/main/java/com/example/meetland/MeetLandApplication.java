2cbf33c4eb4fa356fbb98f9176c178f4c5e93f49
2sssg <73799865+2sssg@users.noreply.github.com>
2021-12-07 01:35:24 +0900

    Initial commit

---
+++ src/main/java/com/example/meetland/MeetLandApplication.java
@@ -0,0 +1,14 @@
+package com.example.meetland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetLandApplication {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        SpringApplication.run(MeetLandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meetland/MeetLandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetLandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
